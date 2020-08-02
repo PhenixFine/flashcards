6d1ee46992581ba97ef3f6d6b8d5679066da5d62
Danny <64429863+PhenixFine@users.noreply.github.com>
2020-08-02 16:31:55 -0400

    Stage 3 of 7 for JetBrains Academy Flashcard project. • strCardNum, strDefinition, strGetDef, and strWrong make use of lambda expressions to return a dynamic string • program was updated to create as many Flashcard objects as the user asks for. After saving all the Flashcards from the user into an array, the user is then challenged to answer each one correctly, informing the user of the correct answer if the user answers wrong.

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,18 @@
-fun main() {
-    val wrong = "Your answer is wrong..."
-    val right = "Your answer is right!"
-    val term = readLine()!!
-    val definition = readLine()!!
-    val answer = readLine()!!
+import utility.getNum
+import utility.getString
 
-    println(if (definition == answer) right else wrong)
+fun main() {
+    val strNumOfCards = "Input the number of cards:"
+    val strCardNum = { num: Int -> "The card #$num:" }
+    val strDefinition = { num: Int -> "The definition of the card #$num:" }
+    val strGetDef = { term: String -> "Print the definition of \"$term\":" }
+    val strCorrect = "Correct answer."
+    val strWrong = { definition: String -> "Wrong answer. The correct one is \"$definition\"." }
+    val numOfCards = getNum(strNumOfCards)
+    val flashcards = Array(numOfCards) { Flashcard(getString(strCardNum(it + 1)), getString(strDefinition(it + 1))) }
+
+    flashcards.forEach {
+        val answer = getString(strGetDef(it.term))
+        println(if (answer == it.definition) strCorrect else strWrong(it.definition))
+    }
 }
