747bf72ff9a057069ba17564e738ef5ff494bb85
Danny <64429863+PhenixFine@users.noreply.github.com>
2020-08-04 16:09:40 -0400

    Stage 5 of 7 for JetBrains Academy Flashcard project. • val flashcards was changed to a top level private value, and made mutable, as the new menu options didn't make it practical to recast it as a Map every time it needed to be changed ( though I'm unsure if would have been better to pass it to the functions when it needed changing, versus, letting the functions directly change it themselves ). • the asking of questions was moved from main() to ask(). Main() is now a menu of options, that directs to corresponding functions based on the user's choice. The main() directs to the following functions: getFlashcard(), removeFlashcard(), import(), export(), and ask() ( val errorAsk() is called instead of ask() if there is no stored flashcards ). • getFlashcard() was changed from original to store one flashcard if the user enters a unique term and definition. • removeFlashcard() removes a flashcard if it is there to be removed. • import() adds flashcards to the current flashcards, overwriting any current flashcards with the same term. If the file is not found, the user is informed. • export() exports the current flashcards to a user given filename. • ask() asks how many questions the user wants to be asked, and then a random stored flashcard is picked for testing the user.

--- src/Main.kt
+++ src/Main.kt
@@ -1,43 +1,111 @@
 import utility.getNum
 import utility.getString
+import java.io.File
+import java.util.*
+
+private val FLASHCARDS = mutableMapOf<String, String>()
 
 fun main() {
+    val strCommands = "Input the action (add, remove, import, export, ask, exit):"
+    val errorAsk = { println("No questions stored to ask.\n") }
+    var exit = false
+
+    do {
+        when (getString(strCommands).toLowerCase()) {
+            "add" -> getFlashcard()
+            "remove" -> removeFlashcard()
+            "import" -> import()
+            "export" -> export()
+            "ask" -> if (FLASHCARDS.isEmpty()) errorAsk() else ask()
+            "exit" -> exit = true
+        }
+    } while (!exit)
+    println("Bye Bye!")
+}
+
+private fun getFlashcard() {
+    val strTermExists = { term: String -> "The card \"$term\" already exists.\n" }
+    val strDefExists = { def: String -> "The definition \"$def\" already exists.\n" }
+    val term = getString("The card:")
+    if (FLASHCARDS.containsKey(term)) {
+        println(strTermExists(term))
+        return
+    }
+    val definition = getString("The definition of the card:")
+    if (FLASHCARDS.containsValue(definition)) {
+        println(strDefExists(definition))
+        return
+    }
+    FLASHCARDS[term] = definition
+    println("The pair (\"$term\":\"$definition\") has been added.\n")
+}
+
+private fun removeFlashcard() {
+    val strRemoved = "The card has been removed.\n"
+    val term = getString("Which card:")
+    val strNotFound = "Can't remove \"$term\": there is no such card.\n"
+
+    println(
+        if (FLASHCARDS.containsKey(term)) {
+            FLASHCARDS.remove(term)
+            strRemoved
+        } else strNotFound
+    )
+}
+
+private fun import() {
+    val fileName = getString("File name:")
+    try {
+        var count = 0
+        var term = ""
+        File(fileName).forEachLine {
+            count++
+            if (count % 2 != 0) term = it else FLASHCARDS[term] = it
+        }
+        println("${count / 2} cards have been loaded\n")
+    } catch (e: Exception) {
+        println("File not found.\n")
+    }
+}
+
+private fun export() {
+    val myFile = File(getString("File name:"))
+
+    try {
+        var hold = ""
+        for ((term, definition) in FLASHCARDS) hold += "$term\n$definition\n"
+        myFile.writeText(hold.trim())
+        println("${FLASHCARDS.size} cards have been saved.")
+    } catch (e: Exception) {
+        println("There was an error in writing your file, please try again.\n")
+    }
+}
+
+private fun ask() {
+    val tries = getNum("How many times to ask?")
     val strGetDef = { term: String -> "Print the definition of \"$term\":" }
     val strCorrect = "Correct answer."
     val strWrong = { definition: String -> "Wrong answer. The correct one is \"$definition\"" }
     val strWrongTerm = { term: String -> ", you've just written the definition of \"$term\"." }
-    val flashcards = getFlashCards()
+    val terms = FLASHCARDS.keys.toTypedArray()
+    val random = { (0..terms.lastIndex).random() }
 
-    for ((term, definition) in flashcards) {
+    repeat(tries) {
+        val term = terms[random()]
         val answer = getString(strGetDef(term))
+        val definition = FLASHCARDS[term]!!
         println(
             when {
                 answer == definition -> strCorrect
-                flashcards.containsValue(answer) -> {
-                    "${strWrong(definition)}${strWrongTerm(flashcards.keys.first { answer == flashcards[it] })}"
+                FLASHCARDS.containsValue(answer) -> {
+                    strWrong(definition) + strWrongTerm(FLASHCARDS.keys.first { answer == FLASHCARDS[it] })
                 }
                 else -> "${strWrong(definition)}."
             }
         )
     }
+    println()
 }
 
-fun getFlashCards(): Map<String, String> {
-    val strNumOfCards = "Input the number of cards:"
-    val strCardNum = { num: Int -> "The card #$num:" }
-    val strDefinition = { num: Int -> "The definition of the card #$num:" }
-    val strTermExists = { term: String -> "The card \"$term\" already exists. Try again:" }
-    val strDefExists = { def: String -> "The definition \"$def\" already exists. Try again:" }
-    val numOfCards = getNum(strNumOfCards)
-    val flashcards = mutableMapOf<String, String>()
-
-    for (i in 1..numOfCards) {
-        var term = getString(strCardNum(i))
-        while (flashcards.containsKey(term)) term = getString(strTermExists(term))
-        var definition = getString(strDefinition(i))
-        while (flashcards.containsValue(definition)) definition = getString(strDefExists(definition))
-        flashcards[term] = definition
-    }
-
-    return flashcards
-}
+// this useful random function was found on Stack Overflow
+private fun IntRange.random() = Random().nextInt(endInclusive + 1 - start) + start
